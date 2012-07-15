a1a892b064b973e16e2e16241570344676b153ce
Semyon Perepelitsa <sema@sema.in>
2012-07-15 11:40:15 +0800

    Refactoring update, handle connection failures

--- game.rb
+++ game.rb
@@ -22,12 +22,6 @@ class BasicPlayer < Chingu::GameObject
   traits :velocity
   attr_reader :id
 
-  def self.load(attributes)
-    player = all.find{ |p| p.id.to_s == attributes.first }
-    player ||= create
-    player.attributes = attributes
-  end
-
   def id=(id)
     @id = if id.is_a?(BSON::ObjectId)
       id
@@ -38,18 +32,16 @@ class BasicPlayer < Chingu::GameObject
 
   def initialize(opts={})
     super(opts.merge(image: "player.png", zorder: 100))
-    @id = BSON::ObjectId.new
+    @id = opts.fetch(:id, BSON::ObjectId.new)
   end
 
   def attributes
-    [id.to_s].concat(super)
+    [x, y]
   end
 
   def attributes=(array)
-    self.id = array.shift
     self.x = array[0]
     self.y = array[1]
-    # super
   end
 end
 
@@ -81,37 +73,69 @@ end
 
 class Node < Chingu::BasicGameObject
   traits :timer
-  DELAY = 20
+  RECONNECT_AFTER = 500
+  SEND_DELAY = 20
 
   def initialize(controlled)
     super()
     @buffer = ""
-    @socket = TCPSocket.open('localhost', 4466)
+    connect
     @controlled = controlled
-    every(DELAY) { send_data }
-  end
-
-  def update
-    super
-    receive_updates
+    @remote = {}
   end
 
   def destroy
     super
-    @socket.close
+    @socket.close if @socket
   end
 
   private
 
-  def send_data
-    @controlled.each do |object|
-      @socket.puts JSON.dump(object.attributes)
+  def connect
+    return if @socket
+    @socket = TCPSocket.open('localhost', 4466)
+  rescue Errno::ECONNREFUSED
+    $logger.info "Retrying the connection"
+    after(RECONNECT_AFTER){ connect }
+  else
+    $logger.info "Connected"
+    every(20, name: :updates) do
+      send_updates
+      receive_updates
     end
   end
 
+  def connection_lost
+    return if @socket.nil?
+    stop_timer(:updates)
+    @socket = nil
+    $logger.info "Connection to the server lost"
+    connect
+  end
+
+  def connection_totally_lost
+    $logger.info "Connection to the server totally lost"
+    @remote.each{ |id, object| object.destroy }
+    destroy
+  end
+
+  def send_updates
+    @controlled.each do |object|
+      send_data(object)
+    end
+  end
+
+  def send_data(object)
+    @socket.puts JSON.dump([object.id.to_s] << object.attributes) if @socket
+  rescue Errno::EPIPE
+    connection_lost
+  end
+
   def receive_updates
-    @buffer << @socket.read_nonblock(1000)
+    @buffer << @socket.read_nonblock(1000) if @socket
   rescue Errno::EAGAIN
+  rescue Errno::ECONNRESET, EOFError
+    connection_lost
   else
     while message = @buffer.slice!(/.*\n/)
       receive_data(message)
@@ -119,14 +143,15 @@ class Node < Chingu::BasicGameObject
   end
 
   def receive_data(data)
-    BasicPlayer.load(JSON.parse(data))
+    id, attributes = JSON.parse(data)
+    player = @remote[id] ||= BasicPlayer.create(id: id)
+    player.attributes = attributes
   end
 end
 
 class Play < Chingu::GameState
   def setup
     @player = Player.create(x: rand($window.width), y: rand($window.height))
-    # @remote = RemotePlayer.create
     Node.create([@player])
   end
 
