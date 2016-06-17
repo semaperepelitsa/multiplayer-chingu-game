f4b84c3b33114a90b2179159a16f08a6083a48d5
Semyon Perepelitsa <sema@sema.in>
2016-06-18 02:10:41 +0300

    Specify required gems.

--- run.rb
+++ run.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+require 'bundler/setup'
 $LOAD_PATH.unshift(File.expand_path('../game', __FILE__))
 $LOAD_PATH.unshift(File.expand_path('../game/states', __FILE__))
 $LOAD_PATH.unshift(File.expand_path('../game/objects', __FILE__))
